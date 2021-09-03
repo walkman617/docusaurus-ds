ec2cbda9445b006d1a9ee74eeae9f2bd00d88342
Neo Shi <shiwenxuan2003@gmail.com>
2021-09-04 00:23:48 +0800

    Update HomepageFeatures.js

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,32 +4,29 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Data preparation',
     Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Data science projects start by asking the right business questions and collecting and preparing data. Success in the later phases is dependent on this early stage.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Data exploration',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Once your data is in the right format to work with, you can conduct the next phase in the data analysis process. This initial exploration of the dataset is critical.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Model development',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Here, the data you've prepared is brought into the data science toolset and the results begin to shed some light on previously identified business problems.
       </>
     ),
   },
