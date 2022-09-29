bd7e0dbaabf01860b9c4573c7b59330a0fea6917
Neo Shi <shiwenxuan2003@gmail.com>
2022-09-29 15:27:58 +0800

    Update HomepageFeatures.js

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -4,29 +4,29 @@ import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
   {
-    title: 'Data preparation',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-        Data science projects start by asking the right business questions and collecting and preparing data. Success in the later phases is dependent on this early stage.
-      </>
-    ),
-  },
-  {
-    title: 'Data exploration',
+    title: '数据科学',
     Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Once your data is in the right format to work with, you can conduct the next phase in the data analysis process. This initial exploration of the dataset is critical.
+        数据科学是使用诸如统计、机器学习、数据挖掘和预测性分析等方法从海量数据中提取知识的实践方法。
       </>
     ),
   },
   {
-    title: 'Model development',
+    title: '数据科学从业者',
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        使用高级数据科学方法和工具，利用统计科学、机器学习技术和特定于行业的数据集来实施独特的数据模型，从而解决各行各业中极具挑战的各种难题。
+      </>
+    ),
+  },
+  {
+    title: '设计思维',
     Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Here, the data you've prepared is brought into the data science toolset and the results begin to shed some light on previously identified business problems.
+        设计思维允许数据科学家等专家以通用语言与各种利益相关者（非专家）进行交流，以便在开始工作之前调整、构思和建立同理心并可视化结果。
       </>
     ),
   },
